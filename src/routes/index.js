0b1fba3af38937957846aec7696e47a5d3174940
Muhammad Usama Rabani <usama.rabbani@invozone.com>
2022-04-10 20:12:03 +0500

    directoru setting up

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/' />
+				<Route path='/login' />
+				<Route path='/register' />
+			</Switch>
+		</Router>
+	);
+};
+
+export default Routes;
