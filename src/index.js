61aff86dd0dc870d6305ef1794bb3608e8f93d17
Muhammad Usama Rabani <usama.rabbani@invozone.com>
2022-04-11 13:17:12 +0500

    register component ui

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import reportWebVitals from './reportWebVitals';
 import Routes from './routes';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
 	<React.StrictMode>
