2e0c8d01384b2534ff0636715493740ec73025ee
Muhammad Usama Rabani <usama.rabbani@invozone.com>
2022-04-11 14:37:27 +0500

    versions change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import Routes from './routes';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
@@ -16,4 +13,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
