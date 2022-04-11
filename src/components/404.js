c012ad858208e18a465918917807bdf206fbf7c6
Muhammad Usama Rabani <usama.rabbani@invozone.com>
2022-04-11 10:52:57 +0500

    404 page and react-bootstrap

--- src/components/404.js
+++ src/components/404.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
 	return (
-		<div className='d-flex justify-content-center'>
+		<div className='mt-5 d-flex justify-content-center'>
 			<h1>404! Page Not Found</h1>
 		</div>
 	);
