3f93f37c17507ff08f49438838e2088705901084
Muhammad Usama Rabani <usama.rabbani@invozone.com>
2022-04-12 14:39:15 +0500

    firebase login with database

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -9,7 +9,8 @@ import {
 	Icon,
 } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-import firebase from '@components/firebase';
+import { firebase } from '@components/firebase';
+import md5 from 'md5';
 
 class Register extends Component {
 	state = {
@@ -17,6 +18,37 @@ class Register extends Component {
 		email: '',
 		password: '',
 		passwordConfirmation: '',
+		errors: [],
+		loading: false,
+		usersRef: firebase.database().ref('users'),
+	};
+	isFormValid = () => {
+		let errors = [];
+		let error;
+		if (this.isFormEmpty(this.state)) {
+			error = { message: 'Fill in all the fields' };
+			this.setState({ errors: errors.concat(error) });
+		} else if (!this.isPasswordValid(this.state)) {
+			error = { message: 'Password is invalid' };
+			this.setState({ errors: errors.concat(error) });
+		} else return true;
+	};
+	isPasswordValid = ({ password, passwordConfirmation }) => {
+		if (password.length < 6 || passwordConfirmation.length < 6) {
+			return false;
+		} else if (password !== passwordConfirmation) {
+			return false;
+		} else {
+			return true;
+		}
+	};
+	isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+		return (
+			!username.length ||
+			!email.length ||
+			!password.length ||
+			!passwordConfirmation.length
+		);
 	};
 	handleChange = (event) => {
 		this.setState({
@@ -25,14 +57,57 @@ class Register extends Component {
 	};
 	handleSubmit = (event) => {
 		event.preventDefault();
+		if (this.isFormValid()) this.setState({ loading: true, errors: [] });
 		firebase
 			.auth()
 			.createUserWithEmailAndPassword(this.state.email, this.state.password)
-			.then((createdUser) => console.log('createdUser ==>', createdUser))
-			.catch((error) => console.log('error ==>', error));
+			.then((createdUser) => {
+				createdUser.user
+					.updateProfile({
+						displayName: this.state.username,
+						photoURL: `http://gravatar.com/avatar/${md5(
+							createdUser.user.email
+						)}?d=identicon`,
+					})
+					.then(() => {
+						this.saveUser(createdUser).then(() => {
+							console.log('user saved!');
+						});
+					})
+					.catch((err) => {
+						console.log('err', err);
+						this.setState({
+							errors: this.state.errors.concat(err),
+							loading: false,
+						});
+					});
+			})
+			.catch((error) => {
+				this.setState({
+					loading: false,
+					errors: this.state.errors.concat(error),
+				});
+			});
+	};
+	displayErrors = (errors) =>
+		errors.map((error, index) => <p key={index}>{error.message}</p>);
+
+	handleInputError = (errors, inputName) => {
+		return errors.some((error) =>
+			error.message.toLowerCase().includes(inputName)
+		)
+			? 'error'
+			: '';
+	};
+	saveUser = (createdUser) => {
+		return this.state.usersRef.child(createdUser.user.uid).set({
+			name: createdUser.user.displayName,
+			avatar: createdUser.user.photoURL,
+		});
 	};
 	render() {
-		const { username, email, password, passwordConfirmation } = this.state;
+		const { username, email, password, passwordConfirmation, errors, loading } =
+			this.state;
 		return (
 			<Grid textAlign='center' verticalAlign='middle'>
 				<Grid.Column style={{ maxWidth: 450 }}>
@@ -52,7 +127,6 @@ class Register extends Component {
 								type='text'
 								value={username}
 							/>
-
 							<Form.Input
 								fluid
 								name='email'
@@ -62,8 +136,8 @@ class Register extends Component {
 								onChange={this.handleChange}
 								type='email'
 								value={email}
+								className={this.handleInputError(errors, 'email')}
 							/>
-
 							<Form.Input
 								fluid
 								name='password'
@@ -73,8 +147,8 @@ class Register extends Component {
 								onChange={this.handleChange}
 								type='password'
 								value={password}
+								className={this.handleInputError(errors, 'password')}
 							/>
-
 							<Form.Input
 								fluid
 								name='passwordConfirmation'
@@ -84,13 +158,22 @@ class Register extends Component {
 								onChange={this.handleChange}
 								type='password'
 								value={passwordConfirmation}
+								className={this.handleInputError(errors, 'password')}
 							/>
-
-							<Button color='orange' fluid size='large'>
+							<Button
+								color='orange'
+								disabled={loading}
+								className={loading ? 'loading' : ''}
+								fluid
+								size='large'
+							>
 								Submit
 							</Button>
 						</Segment>
 					</Form>
+					{errors.length > 0 && (
+						<Message error>{this.displayErrors(errors)}</Message>
+					)}
 					<Message>
 						Already a user? <Link to='/login'>Login</Link>
 					</Message>
