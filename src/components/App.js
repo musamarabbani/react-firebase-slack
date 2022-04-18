7e688ddf7ecbcc11dbdcd0f072b8862938cae5d3
Muhammad Usama Rabani <usama.rabbani@invozone.com>
2022-04-18 15:48:15 +0500

    app layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import ColorPanel from './ColorPanel';
+import Messages from './Messages';
+import MetaPanel from './MetaPanel';
+import SidePanel from './SidePanel';
 import './App.css';
 
 function App() {
-	return <div>This is Main Component</div>;
+	return (
+		<Grid columns='equal' className='app' style={{ background: '#eee' }}>
+			<ColorPanel />
+			<SidePanel />
+			<Grid.Column style={{ marginLeft: 320 }}>
+				<Messages />
+			</Grid.Column>
+			<Grid.Column width={4}>
+				<MetaPanel />
+			</Grid.Column>
+		</Grid>
+	);
 }
 
 export default App;
