04ed58fbfe611d0f417f20ae00c3d07ff453d372
Muhammad Usama Rabani <usama.rabbani@invozone.com>
2022-04-18 15:39:12 +0500

    loading and redux setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Provider from 'react-redux/lib/components/Provider';
+import store from './redux/store';
 import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Routes />
+		<Router>
+			<Provider store={store}>
+				<Routes />
+			</Provider>
+		</Router>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
