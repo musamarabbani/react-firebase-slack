6f02d05e59c48b2525404d634d62b789d844ab89
musamarabbani <usama.rabbani@invozone.com>
2022-06-10 15:25:01 +0500

    resolve conflicts

--- src/components/SidePanel/index.js
+++ src/components/SidePanel/index.js
@@ -3,7 +3,7 @@ import { Menu } from 'semantic-ui-react';
 import Channels from './Channels';
 import UserPanel from './UserPanel';
 import DirectMessages from './DirectMessages';
-import Starred from 'react';
+import Starred from './Starred';
 
 class SidePanel extends React.Component {
   render() {
